6ab8035c704e360f06322c4c5484503d9cf88b43
Dmitrii Glushenko <warstream55@gmail.com>
2025-01-15 16:56:11 +0300

    update urls

--- src/main.js
+++ src/main.js
@@ -3,9 +3,9 @@ import App from './App.svelte';
 const app = new App({
 	target: document.body,
 	props: {
-		open_weather_url: 'https://functions.yandexcloud.net/d4e56ndpfn2i6b48m7c2/',
-		accu_weather_url: 'https://functions.yandexcloud.net/d4e0j0ohqbqmattnn312/',
-		cache_url: 'https://functions.yandexcloud.net/d4encdck6aphnkb3gatm/'
+		open_weather_url: 'https://functions.yandexcloud.net/d4e56ndpfn2i6b48m7c2/?',
+		accu_weather_url: 'https://functions.yandexcloud.net/d4e0j0ohqbqmattnn312/?',
+		cache_url: 'https://functions.yandexcloud.net/d4encdck6aphnkb3gatm/?'
 	}
 });
 
